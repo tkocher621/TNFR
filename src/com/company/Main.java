bbc7f3d10cfda54233a960820866638ccfe99544
tk304 <tk304@pvsd.org>
2018-09-24 10:54:35 -0400

    boiling water

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,31 @@ import java.util.*;
 
 public class Main {
 
+    public static String ToHex(int i)
+    {
+        return Integer.toHexString(i);
+    }
+    public static String ToBinary(int i)
+    {
+        return Integer.toBinaryString(i);
+    }
+    public static String ToOctal(int i)
+    {
+        return Integer.toOctalString(i);
+    }
+    public static String ToDecimal(int i)
+    {
+        return "egg";
+    }
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
 
-        
+        char[] inputChar = input.toCharArray();
+
+
 
     }
 }
