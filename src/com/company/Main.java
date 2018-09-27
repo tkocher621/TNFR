9e60e308d49152693698e7d7d6fadbb8ae6da5fa
Todd <tk304@pvsd.org>
2018-09-27 17:31:59 -0400

    you ever realized that task manager catching on fire is the computer equivalent of a fire extinguisher catching on fire?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,31 @@
 package com.company;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.*;
 
 public class Main {
 
-    public static String ToHex(int i)
+    public static String ToHex(char i)
     {
         return Integer.toHexString(i);
     }
-    public static String ToBinary(int i)
+    public static String ToBinary(char i)
     {
         return Integer.toBinaryString(i);
     }
-    public static String ToOctal(int i)
+    public static String ToOctal(char i)
     {
         return Integer.toOctalString(i);
     }
-    public static int ToDecimal(char i)
+    public static String ToDecimal(char i)
     {
-        return (int)i;
+        return Integer.toString((int)i);
+    }
+
+    public static int GetRandomNumber(int min, int max)
+    {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
     }
 
     public static void main(String[] args) {
@@ -27,7 +35,48 @@ public class Main {
 
         char[] inputChar = input.toCharArray();
 
-        System.out.println(ToDecimal('c'));
+        Map<Integer, String> strDict = new HashMap<Integer, String>();
 
+        int a = GetRandomNumber(1, 5); // Position in sequence
+        int b = GetRandomNumber(0, inputChar.length-1); // Position in char array
+        int c = 0; // Count
+        while (c < inputChar.length)
+        {
+            if (a == 1)
+            {
+                strDict.put(b, ToBinary(inputChar[b]));
+                System.out.println(inputChar[b] + " - " + ToBinary(inputChar[b]) + " - " + b);
+            }
+            else if (a == 2)
+            {
+                strDict.put(b, ToOctal(inputChar[b]));
+                System.out.println(inputChar[b] + " - " + ToOctal(inputChar[b]) + " - " + b);
+            }
+            else if (a == 3)
+            {
+                strDict.put(b, ToDecimal(inputChar[b]));
+                System.out.println(inputChar[b] + " - " + ToDecimal(inputChar[b]) + " - " + b);
+            }
+            else if (a == 4)
+            {
+                strDict.put(b, ToHex(inputChar[b]));
+                System.out.println(inputChar[b] + " - " + ToHex(inputChar[b]) + " - " + b);
+            }
+            else if (a == 5)
+            {
+                strDict.put(b, Character.toString(inputChar[b]));
+                System.out.println(Integer.toString(inputChar[b]));
+            }
+            c++;
+            b = (b == inputChar.length-1) ? 0 : b+1;
+            a = (a == 5) ? 1 : a+1;
+        }
+
+        String fs = "";
+
+        for(Map.Entry<Integer, String> entry : strDict.entrySet())
+            fs+= entry.getValue();
+
+        System.out.println(fs);
     }
 }
